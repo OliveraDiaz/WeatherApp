8c9cca57f694b0a90703ebd25f0ee6498f9f5a0c
OliveraDiaz <130876122+OliveraDiaz@users.noreply.github.com>
2023-07-25 01:47:42 -0500

    got search bar working.removed hardcode

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+
+var form = document.getElementById('form')
+form.addEventListener('submit',function(event){
+
+    event.preventDefault()
+    var city = document.getElementById('city').value
+    console.log(city)
+})
 // get api
 // var temp = document.getElementbyId("currentWeatherCard")
+// var searchBtn = document.getElementById("#fetchDataBtn")
 
-function getApi(query){
+
+function getApi(city){
     // Save API in a variable RequestURL
-    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=0edeb8b579e8e284e65b6f8bcc481965"
+
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=0edeb8b579e8e284e65b6f8bcc481965";
 // fetch API data
+
     fetch(requestUrl)
     // // once fetched
     .then((response) => response.json())
@@ -12,33 +24,47 @@ function getApi(query){
 
     //     return response.json();
        
-    }
-    getApi()
+}
+getApi()
  
 
 function appendWeather(weather){
     console.log(weather);
  
-    var currentWeather = document.querySelector("#currentWeatherCard");
+    var currentWeatherCity = document.querySelector("#currentWeatherCityCard");
+
 
     var city = document.createElement("h1");
     city.textContent= weather.name;
-    currentWeather.append(city);
+    currentWeatherCity.append(city);
+    city.classList.add("text-3xl", "font-extrabold");
+
+    var currentWeatherDetails = document.querySelector("#currentWeatherDetailsEl");
 
     var temp = document.createElement("p");
-    temp.textContent= weather.main.temp;
-    currentWeather.append(temp);
+    temp.textContent= "Temperature " + weather.main.temp + "° F";
+    currentWeatherDetails.append(temp) ;
+    temp.classList.add( "text-xl" , "p-2" , "mt-10");
 
     var wind = document.createElement("p");
-    wind.textContent= weather.wind.speed;
-    currentWeather.append(wind);
+    wind.textContent= "Wind " + weather.wind.speed + " MPH";
+    currentWeatherDetails.append(wind);
+    wind.classList.add( "text-xl", "p-2");
 
     var humidity = document.createElement("p");
-    humidity.textContent= weather.main.humidity;
-    currentWeather.append(humidity);
+    humidity.textContent= "Humidity " + weather.main.humidity + "%";
+    currentWeatherDetails.append(humidity);
+    humidity.classList.add( "text-xl" , "p-2" );
 //     // create p for temp,, wind, humidity
-details.append("")
+
 }
-appendWeather()
-temp.append(city);
-// fetchDataBtn.addEventListener("click", getAPI);
+// appendWeather(city)
+//incert city name
+// var button = document.getElementById("fetchDataBtn");
+// // searchBtn.addEventListener("click", getApi, appendWeather);
+document.querySelector(".fetchDataBtn").addEventListener("click", function (event) {
+    event.preventDefault();
+    var city = document.getElementById("city").value;
+    getApi(city);
+    appendWeather;
+  });
