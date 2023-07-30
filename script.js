e241db99b02e0697501bde4926efe7258544354c
OliveraDiaz <130876122+OliveraDiaz@users.noreply.github.com>
2023-07-30 02:15:29 -0500

    added to read me and style

--- script.js
+++ script.js
@@ -24,14 +24,8 @@ function getApi() {
     event.preventDefault();
     getApi();
   });
-  
-  
-document.querySelector(".fetchDataBtn").addEventListener("click", function (event) {
-    event.preventDefault();
-    
-    console.log(city);
-    getApi();
-});
+   
+
 
 function appendWeather(weather) {
     console.log(weather);
@@ -39,31 +33,57 @@ function appendWeather(weather) {
     var cityElement = document.querySelector(".city");
     cityElement.textContent = weather.city.name;
     
+    var day1Weather = document.getElementById("mainIcon");
+    day1Weather.src = "http://openweathermap.org/img/w/" + weather.list[0].weather[0].icon + ".png";
+
     var tempElement = document.querySelector(".temp");
     tempElement.textContent =  weather.list[0].main.temp + "°F";
 
     var windElement = document.querySelector(".wind");
     windElement.textContent = weather.list[0].wind.speed + "MPG";
+  
 
     var humidityElement = document.querySelector(".humidity");
     humidityElement.textContent = weather.list[0].main.humidity + "% humidity";
-
+    
     var dateElement = document.querySelector(".date");
     dateElement.textContent = weather.list[0].dt_txt;
-
+    
     var tempElement1 = document.querySelector(".cardForcast");
     tempElement1.textContent = "Temperature: " + weather.list[1].main.temp +"°F    " + " Wind: " + weather.list[1].wind.speed + "MPG  " +"Humidity " + weather.list[1].main.humidity + "%" ;
-
+    var day1Weather = document.getElementById("icon1");
+    day1Weather.src = "http://openweathermap.org/img/w/" + weather.list[1].weather[0].icon + ".png";
+    
     var tempElement1 = document.querySelector(".cardForcast1");
     tempElement1.textContent = "Temperature: " + weather.list[2].main.temp +"°F    " + " Wind: " + weather.list[2].wind.speed + "MPG  " + "Humidity " + weather.list[2].main.humidity + "%";
+    var day1Weather = document.getElementById("icon2");
+    day1Weather.src = "http://openweathermap.org/img/w/" + weather.list[2].weather[0].icon + ".png";
+
 
     var tempElement1 = document.querySelector(".cardForcast2");
     tempElement1.textContent = "Temperature: " + weather.list[3].main.temp +"°F    " + " Wind: " + weather.list[3].wind.speed + "MPG  " +"Humidity " + weather.list[3].main.humidity + "%" ;
+    var day1Weather = document.getElementById("icon3");
+    day1Weather.src = "http://openweathermap.org/img/w/" + weather.list[3].weather[0].icon + ".png";
+
   
     var tempElement1 = document.querySelector(".cardForcast3");
     tempElement1.textContent =  "Temperature: " + weather.list[4].main.temp +"°F    " + " Wind: "+  weather.list[4].wind.speed + "MPG  " +"Humidity " + weather.list[4].main.humidity + "%" ;
+    var day1Weather = document.getElementById("icon4");
+    day1Weather.src = "http://openweathermap.org/img/w/" + weather.list[2].weather[0].icon + ".png";
+
 
     var tempElement1 = document.querySelector(".cardForcast4");
     tempElement1.textContent = "Temperature: " + weather.list[5].main.temp +"°F    " + " Wind: "+weather.list[5].wind.speed + "MPG  " + "Humidity " + weather.list[5].main.humidity + "%";
+    var day1Weather = document.getElementById("icon5");
+    day1Weather.src = "http://openweathermap.org/img/w/" + weather.list[1].weather[0].icon + ".png";
+
   }
 getApi();
+
+
+function appendHistory (input) {
+    var history = document.querySelector(".history");
+    var pElement = document.createElement("p");
+    pElement.textContent = input;
+   history.appendChild(pElement);
+}
